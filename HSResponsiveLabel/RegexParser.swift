691c043798020b244c761bf6a932b404fa4ae033
haeseoklee <haeseoklee.dev@gmail.com>
2022-05-26 00:57:37 +0900

    feat: create RegexParser

---
+++ HSResponsiveLabel/RegexParser.swift
@@ -0,0 +1,20 @@
+//
+//  RegexParser.swift
+//  HSResponsiveLabel
+//
+//  Created by Haeseok Lee on 2022/05/25.
+//
+
+import Foundation
+
+class RegexParser {
+    func parse(
+        from text: String,
+        with kind: URLElementKind
+    ) -> ResponsiveElement? {
+        print(text)
+        print(kind.regexPattern)
+        return nil
+    }
+    
+}
