3edba4b9ab8aefdefc5b513f498bef535f699d84
haeseoklee <haeseoklee.dev@gmail.com>
2022-05-26 00:57:24 +0900

    feat: create HSResponsiveLabel

---
+++ HSResponsiveLabel/HSResponsiveLabel.swift
@@ -0,0 +1,39 @@
+//
+//  HSResponsiveLabel.swift
+//  HSResponsiveLabel
+//
+//  Created by Haeseok Lee on 2022/05/25.
+//
+
+import Foundation
+import UIKit
+
+open class HSResponsiveLabel: UILabel {
+    
+    open var kinds: [ResponsiveElementKind] = []
+    
+    private var identifiableKinds: Set<ResponsiveElementKind> {
+        Set<ResponsiveElementKind>(kinds)
+    }
+    
+    private var textStorage: NSTextStorage = NSTextStorage()
+    
+    private var layoutManager: NSLayoutManager = NSLayoutManager()
+    
+    private var textContainer: NSTextContainer = NSTextContainer()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        textStorage.addLayoutManager(layoutManager)
+        layoutManager.addTextContainer(textContainer)
+    }
+}
