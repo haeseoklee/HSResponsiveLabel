df1d78b7128726ebe4942b3d0780fe51a9f9466f
haeseoklee <haeseoklee.dev@gmail.com>
2022-05-28 18:51:00 +0900

    feat: create RegexBuilderType

---
+++ HSResponsiveLabel/RegexBuilderType.swift
@@ -0,0 +1,13 @@
+//
+//  RegexBuilderType.swift
+//  HSResponsiveLabel
+//
+//  Created by Haeseok Lee on 2022/05/27.
+//
+
+import Foundation
+
+protocol RegexBuilderType {
+
+    func makeRegexPattern() -> String
+}
